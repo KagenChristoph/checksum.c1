12d9dcdbcdea0321f14867bc42c2a9394629e662
KagenChristoph <78060919+KagenChristoph@users.noreply.github.com>
2021-02-15 15:18:24 -0800

    Update ints2bytes.c

--- ints2bytes.c
+++ ints2bytes.c
@@ -1,44 +1,22 @@
-/********************************/
-/* Program Name:                */
-/* Author:                      */
-/* Date:                        */
-/********************************/
-/* Description:                 */
-/* Validation Checks:           */
-/* Enhancements:                */
-/********************************/
-
-#include "stdio.h"
-#include "stdlib.h"
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define max_int (255)
 #define byte char
 
+
 int main (int argc, char * argv[], char ** envp) {
 
-  int count = 10;
-  int sum = 0;   
-  byte checksum; 
-  byte complement;
-  byte buf[10];
-  int fd;
-  size_t nbyte;
-  nbyte = sizeof(buf);
-  int retval;
-
-  /* the following is the prototype for the read system call */
-  retval = read(0,  (void *) &buf,nbyte);  
-  for(int i = 0; i < 10; i++)
-    {
-        int value = buf[i];
-        printf("Numbers Entered: %d \n", value);
-    }
-  
-  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
-  if (checksum != complement ) {
-    fprintf(stderr, "Error Detected!\n"); 
-    return 1;
+   int input;
+   byte value;
+   while  ( ! feof(stdin) ) {
+     scanf("%d", &input);
+     if (input > max_int) {
+       fprintf(stderr, "Error in input!\n");
+       exit(1);
+     }
+     value = (byte) input;
+     write(1, &value, sizeof(byte));
   }
-  return 0;
 }
