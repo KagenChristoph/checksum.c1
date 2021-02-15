73c76604be8c15612ac8dc6f418f368b6afa2ddb
KagenChristoph <78060919+KagenChristoph@users.noreply.github.com>
2021-02-15 15:05:49 -0800

    Update ints2bytes.c

--- ints2bytes.c
+++ ints2bytes.c
@@ -1,22 +1,44 @@
+/********************************/
+/* Program Name:                */
+/* Author:                      */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #define max_int (255)
 #define byte char
 
-
 int main (int argc, char * argv[], char ** envp) {
 
-   int input;
-   byte value;
-   while  ( ! feof(stdin) ) {
-     scanf("%d", &input);
-     if (input > max_int) {
-       fprintf(stderr, "Error in input!\n");
-       exit(1);
-     }
-     value = (byte) input;
-     write(1, &value, sizeof(byte));
+  int count = 10;
+  int sum = 0;   
+  byte checksum; 
+  byte complement;
+  byte buf[10];
+  int fd;
+  size_t nbyte;
+  nbyte = sizeof(buf);
+  int retval;
+
+  /* the following is the prototype for the read system call */
+  retval = read(fd,  (void *) &buf,nbyte);  
+  for(int count = 0; count < 10; count++)
+    {
+        int value = buf[count];
+        printf("Numbers Entered: %d \n", value);
+    }
+  
+  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  if (checksum != complement ) {
+    fprintf(stderr, "Error Detected!\n"); 
+    return 1;
   }
+  return 0;
 }
